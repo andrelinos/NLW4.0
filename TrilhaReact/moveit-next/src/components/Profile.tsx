bb44b5573bc3789fafbbda9d0788f19834265618
Andrelino Silva <andrelinocom@gmail.com>
2021-02-26 22:42:19 -0300

    Finalizado quarto dia

--- TrilhaReact/moveit-next/src/components/Profile.tsx
+++ TrilhaReact/moveit-next/src/components/Profile.tsx
@@ -1,12 +1,15 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../context/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/andrelinos.png" alt="Andrelino Silva"/>
       <div>
         <strong>Andrelino Silva</strong>
-        <p><img src="icons/level.svg" alt=""/> Level 1</p>
+        <p><img src="icons/level.svg" alt=""/> Level {level}</p>
       </div>
     </div>
   )
--- TrilhaReact/moveit-next/src/pages/index.tsx
+++ TrilhaReact/moveit-next/src/pages/index.tsx
@@ -6,22 +6,25 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 
 import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../context/CountdownContext';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-    <ExperienceBar />
+      <ExperienceBar />
 
-    <section>
-      <div>
-        <Profile />
-        <CompletedChallenges />
-        <Countdown />
-      </div>
-      <div>
-        <ChallengeBox />
-      </div>
-    </section>
-  </div>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+    </div>
   )
 }
