c857f5171df0960b679c4172bfeda9d1dc646621
Andrelino Silva <andrelinocom@gmail.com>
2021-02-27 23:15:16 -0300

    Finalizando aplicação

--- TrilhaReact/moveit-next/src/pages/index.tsx
+++ TrilhaReact/moveit-next/src/pages/index.tsx
@@ -1,4 +1,8 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+import { ChallengesProvider } from '../context/ChallengesContext';
+
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
@@ -7,10 +11,26 @@ import { Profile } from '../components/Profile';
 
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from '../context/CountdownContext';
+import React from 'react';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
     <div className={styles.container}>
+
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <CountdownProvider>
@@ -26,5 +46,18 @@ export default function Home() {
         </section>
       </CountdownProvider>
     </div>
+    </ChallengesProvider>
   )
+}
+
+// Carrega antes de contruir a interface (SEO)
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } =  ctx.req.cookies;
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    }
+  }
 }
